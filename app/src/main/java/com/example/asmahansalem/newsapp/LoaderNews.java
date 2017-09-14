292c3f42bc7259413e3d5246f343ce5e36bf698d
Asmahan94 <smh.oo.hn94@gmail.com>
2017-09-14 05:20:55 +0300

    update

--- app/src/main/java/com/example/asmahansalem/newsapp/LoaderNews.java
+++ app/src/main/java/com/example/asmahansalem/newsapp/LoaderNews.java
@@ -4,5 +4,34 @@ package com.example.asmahansalem.newsapp;
  * Created by Asmahan Salem on 9/13/2017.
  */
 
-public class LoaderNews {
+public class LoaderNews extends AsyncTaskLoader<List<News>> {
+
+    /**
+     * Here is a Java code source for class loaderNews With full original notes
+     * <p>
+     * https://github.com/DariaKalashnik/News/blob/master/app/src/main/java/com/example/android/news/NewsLoader.java
+     */
+
+    private String mUrl;
+
+    LoaderNews(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    // background thread
+    @Override
+    public List<News> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // perform the network request and extract the news.
+        return Utils.fetchNews(mUrl);
+    }
 }
